04dae8445430d5848dae4b45d07e1f512ad6cbac
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-11-29 21:44:59 +0000

    Reformat with blue

--- src/srv/PointsSamplingSrv.py
+++ src/srv/PointsSamplingSrv.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import rospy
-from object_search.srv import LocationSampling, LocationSamplingRequest, LocationSamplingResponse
+from object_search.srv import (
+    LocationSampling,
+    LocationSamplingRequest,
+    LocationSamplingResponse,
+)
+
 
 def findNearestPoint(point, cordsLst):
     maxProjScope = max(point[0], point[1])
@@ -10,7 +15,7 @@ def findNearestPoint(point, cordsLst):
         for nextY in range(min(i + 2, point[1])):
             if cordsLst[point[0] - i][point[1] + nextY] == 1:
                 return [point[0] - i, point[1] + nextY]
-        
+
         # check upper stride
         for nextX in range(min(i + 1, point[0])):
             if cordsLst[point[0] - i + nextX][point[1] + i] == 1:
@@ -29,12 +34,12 @@ def sampleRoomCB(req: LocationSamplingRequest) -> LocationSamplingResponse:
         cord = [None, None, None]
         for i in range(x, req.deltaBlock[0]):
             for j in range(y, req.deltaBlock[1]):
-                if (req.rasterizedRoom[i][j] == 1):
+                if req.rasterizedRoom[i][j] == 1:
                     cord = req.rasterizedRoom[i][j]
 
         if cord == [None, None, None]:
             return res
-        
+
         # get next cord
         nextX = cord[0] + 0.5 * req.visionScope
         nextY = cord[1] + 0.5 * req.visionScope
@@ -44,7 +49,7 @@ def sampleRoomCB(req: LocationSamplingRequest) -> LocationSamplingResponse:
             return res
         else:
             res.sampledRoom[nearestCurrent[0]][nearestCurrent[1]] = 2
-        
+
         # move current cord to the next one
         x *= 4 * req.visionScope
         y *= 4 * req.visionScope
@@ -52,4 +57,3 @@ def sampleRoomCB(req: LocationSamplingRequest) -> LocationSamplingResponse:
         if nextCord != [None, None]:
             x = nextCord[0]
             y = nextCord[1]
-            
--- src/srv/RasterizationSrv.py
+++ src/srv/RasterizationSrv.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import rospy
 import settings
-from object_search.srv import Rasterization, RasterizationRequest, RasterizationResponse
+from object_search.srv import (
+    Rasterization,
+    RasterizationRequest,
+    RasterizationResponse,
+)
 from os import getcwd
 from math import floor
 import numpy as np
 
+
 def retrieveRoomBoundaries(room: str) -> list[list[float]]:
-    featureFile = getcwd() + "/src/object_search/rooms/rooms.geojson"
+    featureFile = getcwd() + '/src/object_search/rooms/rooms.geojson'
     with open(featureFile) as f:
         features = load(f)['features']
         for feature in features:
@@ -15,29 +20,39 @@ def retrieveRoomBoundaries(room: str) -> list[list[float]]:
                 return feature['geometry']['coordinates']
 
 
-#https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+# https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
 def ccw(p1, p2, p3) -> bool:
-    return (p3[1]-p1[1]) * (p2[0]-p1[0]) > (p2[1]-p1[1]) * (p3[0]-p1[0])
+    return (p3[1] - p1[1]) * (p2[0] - p1[0]) > (p2[1] - p1[1]) * (
+        p3[0] - p1[0]
+    )
 
 
 def intersect(l1, l2):
-    return ccw(l1[0], l2[0], l2[1]) != ccw(l1[1], l2[0], l2[1]) and ccw(l1[0], l1[1], l2[0]) != ccw(l1[0], l1[1], l2[1])
+    return ccw(l1[0], l2[0], l2[1]) != ccw(l1[1], l2[0], l2[1]) and ccw(
+        l1[0], l1[1], l2[0]
+    ) != ccw(l1[0], l1[1], l2[1])
 
 
 def isInsideRoom(point, polygonCords) -> bool:
-    orthLine = ((point[0], point[1]), (point[0], point[1] + settings.POLYGON_CORNOR_FRAME))
+    orthLine = (
+        (point[0], point[1]),
+        (point[0], point[1] + settings.POLYGON_CORNOR_FRAME),
+    )
     inside: bool = False
     for i in range(len(polygonCords)):
-        if intersect(orthLine, ((polygonCords[i]), (polygonCords[(i+1) % len(polygonCords)]))):
+        if intersect(
+            orthLine,
+            ((polygonCords[i]), (polygonCords[(i + 1) % len(polygonCords)])),
+        ):
             inside = not inside
 
     return inside
 
+
 def rasterizationCB(req: RasterizationRequest) -> RasterizationResponse:
     roomBoundaries = retrieveRoomBoundaries(req.room)
 
-
-    boundries = (None, None, None, None) # x left, y top, x right, y bottom
+    boundries = (None, None, None, None)   # x left, y top, x right, y bottom
 
     for i in roomBoundaries:
         # x left
@@ -56,19 +71,17 @@ def rasterizationCB(req: RasterizationRequest) -> RasterizationResponse:
         if not boundries[4] or i[1] < boundries[4]:
             boundries[3] = i[1]
 
-
     # rasterize
-    listXSize = abs(floor((boundries[2] - boundries[0])/req.blockSize))
-    listYSize = abs(floor((boundries[1] - boundries[3])/req.blockSize))
+    listXSize = abs(floor((boundries[2] - boundries[0]) / req.blockSize))
+    listYSize = abs(floor((boundries[1] - boundries[3]) / req.blockSize))
 
-    rasterisedRoom = [[1] * listXSize] * listYSize 
+    rasterisedRoom = [[1] * listXSize] * listYSize
 
     # make 0 where it is outside the room
     for i in range(len(rasterisedRoom)):
         for j in range(len(i)):
-            if not isInsideRoom(rasterisedRoom[i][j]): # fix this
+            if not isInsideRoom(rasterisedRoom[i][j]):   # fix this
                 rasterisedRoom[i][j] = 0
-
 
     res = RasterizationResponse()
     res.deltaBlock = [listXSize, listYSize]
